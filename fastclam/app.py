52cf9fbaadf152d4834d8ee724b62a2352cffac4
Harpo Harbert <ryan_harbert@wgbh.org>
2023-03-17 11:20:14 -0700

    Simplify /pipeline to run all files to single MMIF

--- fastclam/app.py
+++ fastclam/app.py
@@ -51,46 +51,43 @@ def generate_source(files: Inputs) -> dict:
 
 @app.post('/pipeline')
 def run_pipeline(pipeline: Pipeline) -> list | dict | str:
-    """Run a list of media through a list of apps"""
-    results = []
+    """Run a list of media through a list of apps
+
+    Sources all input files into the same input MMIF"""
     log.info(f'Starting pipeline {pipeline}')
-    for media in pipeline.files:
-        input_mmif = generate_source(Inputs(files=[media]))
-        output_mmif = None
-        for app in pipeline.apps:
-            log.debug(f'Running {media} through {app}')
-            response = requests.post(app, json=output_mmif or input_mmif)
-            log.debug(f'received response {response.status_code}, {response.headers}')
-            if response.status_code != 200:
-                log.error(f'Bad response from {app} with {media}')
+    input_mmif = generate_source(Inputs(files=pipeline.files))
+    output_mmif = None
+    for app in pipeline.apps:
+        log.debug(f'Running {pipeline.files} through {app}')
+        response = requests.post(app, json=output_mmif or input_mmif)
+        log.debug(f'received response {response.status_code}, {response.headers}')
+        if response.status_code != 200:
+            log.error(f'Bad response from {app} with {pipeline.files}')
+            log.debug(response.content)
+            raise HTTPException(
+                status_code=response.status_code,
+                detail=f'Unsuccessful response from {app}: {response.status_code} - {response.content}',
+            )
+        content_type = response.headers.get('Content-Type')
+        if 'application/json' in content_type:
+            try:
+                output_mmif = response.json()
+            except JSONDecodeError as e:
+                log.error(f'Error parsing JSON: {e}')
                 log.debug(response.content)
-                raise HTTPException(
-                    status_code=response.status_code,
-                    detail=f'Unsuccessful response from {app}: {response.status_code} - {response.content}',
+        elif 'application/xml' in content_type:
+            try:
+                log.debug('parsing as xml')
+                output_mmif = ElementTree.fromstring(response.content)
+                output_mmif = ElementTree.tostring(output_mmif)
+            except ParseError as e:
+                log.error(f'Error parsing XML: {e}')
+                log.debug(response.content)
+                raise MMIFException(
+                    status_code=499,
+                    detail=f'Failed to parse XML from {app}: {response.status_code} - {response.content}',
                 )
-            content_type = response.headers.get('Content-Type')
-            if 'application/json' in content_type:
-                try:
-                    output_mmif = response.json()
-                    results.append(output_mmif)
-                except JSONDecodeError as e:
-                    log.error(f'Error parsing JSON: {e}')
-                    log.debug(response.content)
-            elif 'application/xml' in content_type:
-                try:
-                    log.debug('parsing as xml')
-                    output_mmif = ElementTree.fromstring(response.content)
-                    results.append(ElementTree.tostring(output_mmif))
-                except ParseError as e:
-                    log.error(f'Error parsing XML: {e}')
-                    log.debug(response.content)
-                    raise MMIFException(
-                        status_code=499,
-                        detail=f'Failed to parse XML from {app}: {response.status_code} - {response.content}',
-                    )
 
     log.info(f'Ran {len(pipeline.files)} files through {len(pipeline.apps)} apps')
 
-    if len(results) == 1:
-        return results[0]
-    return results
+    return output_mmif
