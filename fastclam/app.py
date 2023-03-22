6187f3a16ce32ac7c5b7f28984a1113fe1ec172b
Harpo <ryan_harbert@wgbh.org>
2023-03-22 12:32:43 -0700

    🪓 Parsing (#9)
    * Better logging, better errors
    
    * Move uvicorn to dev dependencies
    
    * Adds black and flake8 to local dev dependencies
    
    * Run launch.json with uvicorn directly using local .venv
    
    * Simplify /pipeline to run all files to single MMIF
    
    * Upload coverage, even on test failures
    
    * Adds codecov.yml to prevent CI failure on low coverage

--- fastclam/app.py
+++ fastclam/app.py
@@ -1,12 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import PlainTextResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from clams.source import generate_source_mmif
-from json import loads
 from pydantic import BaseModel
 from typing import List
 import requests
 from .log import log
 from .version import __VERSION__
 from xml.etree import ElementTree
+from xml.etree.ElementTree import ParseError
+from json import loads
+from json.decoder import JSONDecodeError
 
 
 class Inputs(BaseModel):
@@ -17,7 +21,16 @@ class Pipeline(Inputs):
     apps: List[str]
 
 
+class MMIFException(HTTPException):
+    pass
+
+
 app = FastAPI()
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
 
 
 @app.get('/')
@@ -37,24 +50,44 @@ def generate_source(files: Inputs) -> dict:
 
 
 @app.post('/pipeline')
-def run_pipeline(pipeline: Pipeline, all: bool = False) -> list:
-    """Run a list of media through a list of apps"""
-    results = []
-    log.info(f'Starting pipeline {pipeline}')
-    for media in pipeline.files:
-        mmif = generate_source(Inputs(files=[media]))
+def run_pipeline(pipeline: Pipeline) -> list | dict | str:
+    """Run a list of media through a list of apps
 
-        for app in pipeline.apps:
-            log.debug(f'Running {media} through {app}')
-            response = requests.post(app, json=mmif)
-            assert response.status_code == 200, f'Error with {pipeline}: {response}'
+    Sources all input files into the same input MMIF"""
+    log.info(f'Starting pipeline {pipeline}')
+    input_mmif = generate_source(Inputs(files=pipeline.files))
+    output_mmif = None
+    for app in pipeline.apps:
+        log.debug(f'Running {pipeline.files} through {app}')
+        response = requests.post(app, json=output_mmif or input_mmif)
+        log.debug(f'received response {response.status_code}, {response.headers}')
+        if response.status_code != 200:
+            log.error(f'Bad response from {app} with {pipeline.files}')
+            log.debug(response.content)
+            raise HTTPException(
+                status_code=response.status_code,
+                detail=f'Unsuccessful response from {app}: {response.status_code} - {response.content}',
+            )
+        content_type = response.headers.get('Content-Type')
+        if 'application/json' in content_type:
             try:
-                mmif = response.json()
-            except Exception as e:
-                log.warn(f'Error parsing json {e}')
-                log.debug('Trying to parse as xml')
-                mmif = ElementTree.fromstring(response.content)
-            results.append(mmif)
+                output_mmif = response.json()
+            except JSONDecodeError as e:
+                log.error(f'Error parsing JSON: {e}')
+                log.debug(response.content)
+        elif 'application/xml' in content_type:
+            try:
+                log.debug('parsing as xml')
+                output_mmif = ElementTree.fromstring(response.content)
+                output_mmif = ElementTree.tostring(output_mmif)
+            except ParseError as e:
+                log.error(f'Error parsing XML: {e}')
+                log.debug(response.content)
+                raise MMIFException(
+                    status_code=499,
+                    detail=f'Failed to parse XML from {app}: {response.status_code} - {response.content}',
+                )
 
     log.info(f'Ran {len(pipeline.files)} files through {len(pipeline.apps)} apps')
-    return results
+
+    return output_mmif
